59cff34845d566f87d7191e458a0bfb153e0da8c
schutzer <erichschutz@gmail.com>
2016-02-09 22:18:15 -0500

    commit

---
+++ RealtimeAngular/ext-modules/psGauge/psGaugeDirective.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+angular.module('psGauge').directive('psGauge', ['psWebMetricsService', function (psWebMetricService) {
+    return {
+        scope:{
+            metic:'@metic'
+        },
+        templateUrl: 'ext-modules/psGauge/psGaugeTemplate.html',
+        link: function (scope, el, attrs) {
+
+            scope.initialized = false;
+            scope.title = psWebMetricService.getTitle(scope.metic);
+            scope.options = {
+                width: 200, height: 200,
+                redFrom: 90, redTo: 100,
+                yellowFrom: 75, yellowTo: 99,
+                minorTicks: 5
+            };
+
+           
+            scope.$on('psWebMetricsService-received-data-event', function (evt, data) {
+                if (!scope.initialized) {
+
+                    scope.data = google.visualization.arrayToDataTable([
+                    ['Label', 'Value'],
+                    [scope.title, 0]
+                    ]);
+
+                    scope.chart = new google.visualization.Gauge(el[0]);
+                    scope.initialized = true;
+                }
+                scope.data.setValue(0, 1, Math.round(data[scope.metic]));
+                scope.chart.draw(scope.data, scope.options);
+
+            });
+        }
+    };
+}]);
---
+++ RealtimeAngular/ext-modules/psWebMetricService/psWebMeticsService.js
@@ -0,0 +1,40 @@
+﻿'use strict';
+
+angular.module('psWebMetricsService', []).factory('psWebMetricsService',['$rootScope', function ($rootScope) {
+
+    var hub = $.connection.metricHub;
+
+    hub.client.broadcastMessage = function (time, bandwidthPct, cpuPct, salesAmt, alphaSalesAmt, betaSalesAmt) {
+        $rootScope.$broadcast('psWebMetricsService-received-data-event', {
+            'time': time,
+            'bandwidthPct': bandwidthPct,
+            'cpuPct': cpuPct,
+            'salesAmt': salesAmt,
+            'alphaSalesAmt': alphaSalesAmt,
+            'betaSalesAmt': betaSalesAmt
+        });
+    };
+
+    $.connection.hub
+        .start()
+        .done()
+        .fail(function (data) {
+        alert(data);
+        });
+
+
+    var getTitle = function (metric) {
+        switch (metric) {
+            case 'cpuPct':
+                return 'CPU %';
+            case 'bandwidthPct':
+                return 'Bandwith %';
+        }
+        return undefined;
+    };
+
+    return {
+        getTitle : getTitle
+    };
+
+}]);
